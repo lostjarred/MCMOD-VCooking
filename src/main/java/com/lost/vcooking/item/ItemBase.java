2a39ec2af927d436773c5ced16e82c44d1cad82c
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-08-27 12:27:52 +1000

    update items

--- src/main/java/com/lost/vcooking/item/ItemBase.java
+++ src/main/java/com/lost/vcooking/item/ItemBase.java
@@ -1,7 +1,19 @@
 package com.lost.vcooking.item;
 
+import com.lost.vcooking.VCooking;
+
 import net.minecraft.item.Item;
 
 public class ItemBase extends Item {
-
+	protected String name;
+	
+	public ItemBase(String name){
+		this.name = name;
+		setTranslationKey(name);
+		setRegistryName(name);
+	}
+	
+	public void registerItemModel() {
+		VCooking.proxy.registerItemRenderer(this, 0, name);
+	}
 }
--- src/main/java/com/lost/vcooking/item/ItemFoodBase.java
+++ src/main/java/com/lost/vcooking/item/ItemFoodBase.java
@@ -1,12 +1,20 @@
 package com.lost.vcooking.item;
 
+import com.lost.vcooking.VCooking;
+
 import net.minecraft.item.ItemFood;
 
 public class ItemFoodBase extends ItemFood {
 
-	public ItemFoodBase(int amount, float saturation, boolean isWolfFood) {
+	protected String name;
+	
+	public ItemFoodBase(String name, int amount, float saturation, boolean isWolfFood) {
 		super(amount, saturation, isWolfFood);
-		// TODO Auto-generated constructor stub
+		this.name = name;
+	}
+	
+	public void registerItemModel() {
+		VCooking.proxy.registerItemRenderer(this, 0, name);
 	}
 
 }
