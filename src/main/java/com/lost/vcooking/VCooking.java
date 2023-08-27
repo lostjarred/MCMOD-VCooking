4b5dab801290c742b07c412d88500b91de91ed44
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-08-27 12:15:56 +1000

    spelling error

--- src/main/java/com/lost/vcooking/VCooking.java
+++ src/main/java/com/lost/vcooking/VCooking.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod(modid = VCooking.MODID, name = VCooking.NAME, version = VCooking.VERSION)
 public class VCooking {
 	public static final String MODID = "vcooking";
-	public static final String NAME = "Vanilla Cookings";
+	public static final String NAME = "Vanilla Cooking";
 	public static final String VERSION = "0.1.0";
 	
 	@Mod.Instance(MODID)
