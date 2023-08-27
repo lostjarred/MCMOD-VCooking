5bdac577de4a13ac92f9bab88fdb4d36a2889632
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-08-27 11:38:05 +1000

    add baseitemfood

---
+++ src/main/java/com/lost/vcooking/item/BaseItemFood.java
@@ -0,0 +1,12 @@
+package com.lost.vcooking.item;
+
+import net.minecraft.item.ItemFood;
+
+public class BaseItemFood extends ItemFood {
+
+	public BaseItemFood(int amount, float saturation, boolean isWolfFood) {
+		super(amount, saturation, isWolfFood);
+		// TODO Auto-generated constructor stub
+	}
+
+}
