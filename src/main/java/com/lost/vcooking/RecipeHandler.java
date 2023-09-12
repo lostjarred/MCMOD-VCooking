95a66ef6096ace009589b6f6e48fdc1d917fd34f
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-09-13 06:47:49 +1000

    rename to recipe handler

--- src/main/java/com/lost/vcooking/RecipeHandler.java
+++ src/main/java/com/lost/vcooking/RecipeHandler.java
@@ -3,7 +3,7 @@ package com.lost.vcooking;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-public class SmeltingRecipes {
+public class RecipeHandler {
 		public static void registersmeltingrecipes() {
 			System.out.println("Registering smelting recipes");
 			GameRegistry.addSmelting(ModItems.DOUGH, new ItemStack(ModItems.FLATBREAD), 0);
