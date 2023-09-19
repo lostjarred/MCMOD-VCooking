fab3390b2506a7ea800ded7bc47e4bef415279fd
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-09-19 13:51:52 +1000

    add floure item to itemflour oredict

--- src/main/java/com/lost/vcooking/OredictHandler.java
+++ src/main/java/com/lost/vcooking/OredictHandler.java
@@ -1,7 +1,10 @@
 package com.lost.vcooking;
 
+import net.minecraftforge.oredict.OreDictionary;
+
 public class OredictHandler {
 	public static void RegisterOreDicts() {
 		System.out.println("Registering Ore dicts");
+		OreDictionary.registerOre("itemFlour", ModItems.FLOUR);
 	}
 }
