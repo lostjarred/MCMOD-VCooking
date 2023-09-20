336e8e749541573c4698e09e1a13bf145fbb1844
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-09-20 12:19:46 +1000

    remove bread recipe

--- src/main/java/com/lost/vcooking/RecipeHandler.java
+++ src/main/java/com/lost/vcooking/RecipeHandler.java
@@ -1,6 +1,7 @@
 package com.lost.vcooking;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistryModifiable;
 
@@ -12,6 +13,8 @@ public class RecipeHandler {
 		}
 		
 		public static void removeRecipes(IForgeRegistryModifiable registry) {
-			System.out.println("Removing recipe");
+			System.out.println("Removing recipes");
+			System.out.println("Removing Bread recipe");
+			registry.remove(new ResourceLocation("minecraft:bread") );
 		}
 }
