359037dc5fd3f1b77f281b7b4355186556ace31d
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-09-22 11:13:36 +1000

    update itembasemeta

--- src/main/java/com/lost/vcooking/item/ItemBaseMeta.java
+++ src/main/java/com/lost/vcooking/item/ItemBaseMeta.java
@@ -1,11 +1,30 @@
 package com.lost.vcooking.item;
 
+import java.util.ArrayList;
+
+import com.google.common.collect.Lists;
+
 public class ItemBaseMeta extends ItemBase	{
+	
+	private static ArrayList<metaitem> items = Lists.newArrayList();
 
 	public ItemBaseMeta(String name) {
 		super(name);
 		this.setHasSubtypes(true);
 		this.setMaxDamage(0);
+	}
+	
+	public ItemBaseMeta addMetaItem(String name) {
+		items.add(new metaitem(name));
+		return this;
+	}
+	
+	class metaitem {
+		String itemName;
+		
+		public metaitem(String Name) {
+			this.itemName = Name;
+		}
 	}
 
 }
