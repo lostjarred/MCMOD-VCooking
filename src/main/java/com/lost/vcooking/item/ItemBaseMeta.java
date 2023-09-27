5dc5474451f4e7db986d0e39ac8cb373a5c5eecd
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-09-27 17:15:05 +1000

    add function for translation key

--- src/main/java/com/lost/vcooking/item/ItemBaseMeta.java
+++ src/main/java/com/lost/vcooking/item/ItemBaseMeta.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import com.google.common.collect.Lists;
 
+import net.minecraft.item.ItemStack;
+
 public class ItemBaseMeta extends ItemBase	{
 	
 	private static ArrayList<metaitem> items = Lists.newArrayList();
@@ -13,6 +15,16 @@ public class ItemBaseMeta extends ItemBase	{
 		this.setHasSubtypes(true);
 		this.setMaxDamage(0);
 	}
+	
+	public String getTranslationKey(ItemStack stack)
+    {
+		int metadata = stack.getMetadata();
+		if (metadata > (items.size() - 1) ) {
+			return name + ".error";
+		} else {
+			return name + items.get(metadata).getName();
+		}
+    }
 	
 	public ItemBaseMeta addMetaItem(String name) {
 		items.add(new metaitem(name));
@@ -24,6 +36,10 @@ public class ItemBaseMeta extends ItemBase	{
 		
 		public metaitem(String Name) {
 			this.itemName = Name;
+		}
+		
+		public String getName() {
+			return itemName;
 		}
 	}
 
