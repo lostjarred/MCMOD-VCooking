4549e8d9e114c0c2d4c1f13f0531871369829aa0
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-09-29 10:14:01 +1000

    create itembasemetatool

---
+++ src/main/java/com/lost/vcooking/item/ItemBaseMetaTool.java
@@ -0,0 +1,10 @@
+package com.lost.vcooking.item;
+
+public class ItemBaseMetaTool extends ItemBaseMeta {
+
+	public ItemBaseMetaTool(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
