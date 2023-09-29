733dd708d91b74b608ef92f762e143a37aa6cd5f
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-09-29 11:13:53 +1000

    update itembasemetatool

--- src/main/java/com/lost/vcooking/item/ItemBaseMetaTool.java
+++ src/main/java/com/lost/vcooking/item/ItemBaseMetaTool.java
@@ -1,10 +1,24 @@
 package com.lost.vcooking.item;
 
+import net.minecraft.item.ItemStack;
+
 public class ItemBaseMetaTool extends ItemBaseMeta {
 
 	public ItemBaseMetaTool(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public ItemStack getContainerItem(ItemStack itemStack)
+    {
+        ItemStack item = itemStack.copy();
+        return item;
+    }
+	
+	@Override
+	public boolean hasContainerItem(ItemStack stack) {
+		return true;
 	}
 
 }
