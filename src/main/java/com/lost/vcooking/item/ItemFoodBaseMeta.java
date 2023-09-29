0ba3639f2e1fe277eff4f7049fdd3c0613d6b095
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-09-29 11:33:45 +1000

    add metafood subclass

--- src/main/java/com/lost/vcooking/item/ItemFoodBaseMeta.java
+++ src/main/java/com/lost/vcooking/item/ItemFoodBaseMeta.java
@@ -6,5 +6,29 @@ public class ItemFoodBaseMeta extends ItemFoodBase {
 		super(name, 0, 0, false);
 		// TODO Auto-generated constructor stub
 	}
+	
+	class ItemFoodMeta {
+		String name;
+		int foodvalue;
+		int saturation;
+		
+		ItemFoodMeta(String itemname, int itemfoodvalue, int itemsaturation){
+			this.name = itemname;
+			this.foodvalue = itemfoodvalue;
+			this.saturation = itemsaturation;
+		}
+		
+		public String getName() {
+			return this.name;
+		}
+		
+		public int getFoodValue() {
+			return this.foodvalue;
+		}
+		
+		public int getSaturation() {
+			return this.saturation;
+		}
+	}
 
 }
