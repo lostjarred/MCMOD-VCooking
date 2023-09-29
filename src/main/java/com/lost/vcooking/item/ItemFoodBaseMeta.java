2a8753d845edcf7c69c67ca9937807183a98e28f
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-09-29 11:13:34 +1000

    add itemfoodbasemeta class

---
+++ src/main/java/com/lost/vcooking/item/ItemFoodBaseMeta.java
@@ -0,0 +1,10 @@
+package com.lost.vcooking.item;
+
+public class ItemFoodBaseMeta extends ItemFoodBase {
+
+	public ItemFoodBaseMeta(String name, int amount, float saturation, boolean isWolfFood) {
+		super(name, 0, 0, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
