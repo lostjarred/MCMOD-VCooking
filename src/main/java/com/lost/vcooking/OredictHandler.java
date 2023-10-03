51c2e374fb826017f915926d89b7edae38792a3b
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-10-03 12:27:01 +1100

    add tool oredicts

--- src/main/java/com/lost/vcooking/OredictHandler.java
+++ src/main/java/com/lost/vcooking/OredictHandler.java
@@ -9,5 +9,7 @@ public class OredictHandler {
 		OreDictionary.registerOre("itemFlour", ModItems.FLOUR);
 		OreDictionary.registerOre("itemFlour", new ItemStack(ModItems.ITEMS, 2) );
 		OreDictionary.registerOre("itemDough", new ItemStack(ModItems.ITEMS, 3) );
+		OreDictionary.registerOre("toolMixingBowl", new ItemStack( ModItems.ITEMSTOOL, 0 ));
+		OreDictionary.registerOre("toolMortarPestile", new ItemStack( ModItems.ITEMSTOOL, 1 ));
 	}
 }
