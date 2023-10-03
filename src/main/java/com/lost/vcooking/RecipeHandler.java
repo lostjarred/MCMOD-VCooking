c68f881bfbd254c7020a78fcff84fd28f1c2b48a
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-10-03 17:06:27 +1100

    update recipes

--- src/main/java/com/lost/vcooking/RecipeHandler.java
+++ src/main/java/com/lost/vcooking/RecipeHandler.java
@@ -12,7 +12,7 @@ public class RecipeHandler {
 			GameRegistry.addSmelting(ModItems.DOUGH, new ItemStack(ModItems.FLATBREAD), 0);
 			GameRegistry.addSmelting(ModItems.FILLED_LOAFPAN, new ItemStack(ModItems.FILLED_LOAFPAN_2), 0);
 			GameRegistry.addSmelting(new ItemStack(ModItems.ITEMS, 1 ,0), new ItemStack(ModItems.ITEMSFOOD, 1, 0), 0);
-			GameRegistry.addSmelting(new ItemStack(ModItems.ITEMS, 1 ,3), new ItemStack(ModItems.ITEMS, 1, 4), 0);
+			GameRegistry.addSmelting(new ItemStack(ModItems.ITEMS, 1 ,3), new ItemStack(ModItems.CONTAINERITEMS, 1, 0), 0);
 		}
 		
 		public static void removeRecipes(IForgeRegistryModifiable<IRecipe> registry) {
