727c3b1559e71c494d41c457c9488aedfa520160
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-10-03 14:16:44 +1100

    ammount then metadata !

--- src/main/java/com/lost/vcooking/OredictHandler.java
+++ src/main/java/com/lost/vcooking/OredictHandler.java
@@ -7,9 +7,9 @@ public class OredictHandler {
 	public static void RegisterOreDicts() {
 		System.out.println("Registering Ore dicts");
 		OreDictionary.registerOre("itemFlour", ModItems.FLOUR);
-		OreDictionary.registerOre("itemFlour", new ItemStack(ModItems.ITEMS, 2) );
-		OreDictionary.registerOre("itemDough", new ItemStack(ModItems.ITEMS, 3) );
-		OreDictionary.registerOre("toolMixingBowl", new ItemStack( ModItems.ITEMSTOOL, 0 ));
-		OreDictionary.registerOre("toolMortarPestile", new ItemStack( ModItems.ITEMSTOOL, 1 ));
+		OreDictionary.registerOre("itemFlour", new ItemStack(ModItems.ITEMS, 1, 2) );
+		OreDictionary.registerOre("itemDough", new ItemStack(ModItems.ITEMS, 1, 3) );
+		OreDictionary.registerOre("toolMixingBowl", new ItemStack( ModItems.ITEMSTOOL, 1, 0 ));
+		OreDictionary.registerOre("toolMortarPestile", new ItemStack( ModItems.ITEMSTOOL, 1, 1 ));
 	}
 }
