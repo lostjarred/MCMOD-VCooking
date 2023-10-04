f0e5a605157e621e0830ff7847c317251dc026ec
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-10-04 16:29:56 +1100

    update to use itemstack variables

--- src/main/java/com/lost/vcooking/OredictHandler.java
+++ src/main/java/com/lost/vcooking/OredictHandler.java
@@ -6,8 +6,8 @@ import net.minecraftforge.oredict.OreDictionary;
 public class OredictHandler {
 	public static void RegisterOreDicts() {
 		System.out.println("Registering Ore dicts");
-		OreDictionary.registerOre("itemFlour", new ItemStack(ModItems.ITEMS, 1, 0) );
-		OreDictionary.registerOre("itemDough", new ItemStack(ModItems.ITEMS, 1, 2) );
+		OreDictionary.registerOre("itemFlour", ModItems.FLOUR );
+		OreDictionary.registerOre("itemDough", ModItems.DOUGH );
 		OreDictionary.registerOre("toolMixingBowl", new ItemStack( ModItems.ITEMSTOOL, 1, 0 ));
 		OreDictionary.registerOre("toolMortarPestile", new ItemStack( ModItems.ITEMSTOOL, 1, 1 ));
 	}
--- src/main/java/com/lost/vcooking/RecipeHandler.java
+++ src/main/java/com/lost/vcooking/RecipeHandler.java
@@ -9,8 +9,8 @@ import net.minecraftforge.registries.IForgeRegistryModifiable;
 public class RecipeHandler {
 		public static void registerSmeltingRecipes() {
 			System.out.println("Registering smelting recipes");
-			GameRegistry.addSmelting(new ItemStack(ModItems.ITEMS, 1 ,1), new ItemStack(ModItems.ITEMSFOOD, 1, 0), 0);
-			GameRegistry.addSmelting(new ItemStack(ModItems.ITEMS, 1 ,2), new ItemStack(ModItems.ITEMS, 1 ,3), 0);
+			GameRegistry.addSmelting(ModItems.DOUGH, ModItems.FLATBREAD, 0);
+			GameRegistry.addSmelting(ModItems.FILLEDLOAFPAN, ModItems.COOKEDLOAFPAN, 0);
 		}
 		
 		public static void removeRecipes(IForgeRegistryModifiable<IRecipe> registry) {
