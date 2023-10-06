fa89d2abe1ef0083f3bfb520a6aa7a00b7352e60
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-10-06 11:20:05 +1100

    update vmcitems class

--- src/main/java/com/lost/vcooking/VMCItems.java
+++ src/main/java/com/lost/vcooking/VMCItems.java
@@ -5,10 +5,12 @@ import net.minecraft.item.ItemFood;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class VMCItems {
-	public static final Item BREAD_OVERIDE = new ItemFood(6, 0.6f, false).setTranslationKey("bread").setRegistryName("minecraft", "bread");
+	private static String modname = "minecraft";
+	public static final Item BREAD_OVERIDE = new ItemFood(6, 0.6f, false).setTranslationKey("bread").setRegistryName(modname, "bread");
 	
 	public static void register_Overide(IForgeRegistry<Item> registry) {
 		System.out.println("registering Minecraft Item Overrides");
+		System.out.println("Overriding Bread");
 		registry.register(BREAD_OVERIDE);
 	}
 }
