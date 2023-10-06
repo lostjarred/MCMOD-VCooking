42badbd02594a5c63f6e38782951f86d6cf9e8a9
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-10-06 11:24:56 +1100

    add mushroom stew variable

--- src/main/java/com/lost/vcooking/VMCItems.java
+++ src/main/java/com/lost/vcooking/VMCItems.java
@@ -2,11 +2,15 @@ package com.lost.vcooking;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemSoup;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class VMCItems {
 	private static String modname = "minecraft";
+	
 	public static final Item BREAD_OVERIDE = new ItemFood(6, 0.6f, false).setTranslationKey("bread").setRegistryName(modname, "bread");
+	public static final Item MUSHROOM_STEW_OVERRIDE = new ItemSoup(6).setTranslationKey("mushroomStew").setRegistryName(modname, "mushroom_stew");
+	
 	
 	public static void register_Overide(IForgeRegistry<Item> registry) {
 		System.out.println("registering Minecraft Item Overrides");
